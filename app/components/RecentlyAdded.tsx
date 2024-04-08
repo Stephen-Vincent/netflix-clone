6d0aed0db95df7c77821323638486f7d0601c1f1
Stephen Vincent <stevevincent17@icloud.com>
2024-04-08 20:18:16 +0100

    added the recently added section and modal functionality

---
+++ app/components/RecentlyAdded.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import prisma from "../utils/db";
+import MovieCard from "./MovieCard";
+
+async function getData() {
+  const data = await prisma.movie.findMany({
+    select: {
+      id: true,
+      overview: true,
+      title: true,
+      WatchLists: true,
+      imageString: true,
+      youtubeString: true,
+      age: true,
+      release: true,
+      duration: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 4,
+  });
+
+  return data;
+}
+
+export default async function RecentlyAdded() {
+  const data = await getData();
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 gap-6">
+      {data.map((movie) => (
+        <div key={movie.id} className="relative h-48">
+          <Image
+            src={movie.imageString}
+            alt="movie"
+            className="rounded-sm absolute w-full h-full object-cover"
+            width={500}
+            height={400}
+          />
+
+          <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-125 opacity-0 hover:opacity-100">
+            <div className="bg-gradient-to-b from-transparent via-black/50 to-black z-10 w-full h-full rounded-lg flex items-center justify-center border">
+              <Image
+                src={movie.imageString}
+                className="rounded-lg absolute w-full h-full -z-10 object-cover"
+                width={800}
+                height={800}
+                alt="movie"
+              />
+              <MovieCard
+                movieId={movie.id}
+                overview={movie.overview}
+                title={movie.title}
+                watchListId={movie.WatchLists[0]?.id}
+                youtubeUrl={movie.youtubeString}
+                watchList={movie.WatchLists.length > 0 ? true : false}
+                key={movie.id}
+                age={movie.age}
+                time={movie.duration}
+                year={movie.release}
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,12 @@
-import Navbar from "../components/Navbar";
+import MovieVideo from "../components/MovieVideo";
+import RecentlyAdded from "../components/RecentlyAdded";
 
 export default function HomePage() {
   return (
-    <div>
-      <Navbar />
+    <div className="px-5 lg:p-0">
+      <MovieVideo />
+      <h1 className="text-3xl font-bold">Recently Added</h1>
+      <RecentlyAdded />
     </div>
   );
 }
